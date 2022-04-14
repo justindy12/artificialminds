0149e973b0d7ac0d579d4e9765b0c1e23a01627b
justindy12 <bsjustin106500@yahoo.com>
2022-04-14 20:30:01 +0800

    Committed to iver

--- MyProject/capstone/capstone/settings.py
+++ MyProject/capstone/capstone/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+9beq%fg_)l9sl49bd8ceh_d=e0y#t5c^wba5#7hya7p!+^p6t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['artificial-minds.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
